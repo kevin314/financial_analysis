ff7b7bf338f752feed6278ccf62a15a40d99f953
kevlin3141@gmail.com <kevsaidhi@gmail.com>
2020-01-15 22:09:28 -0600

    print url

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
-import panda
+import pandas
 
-
-page = requests.get('https://www.sec.gov/cgi-bin/browse-edgar?CIK=tsla&Find=Search&owner=exclude&action=getcompany'
-                    '&start=0&count=100')
+ticker = input("Enter company ticker symbol: ")
+page = requests.get('https://www.sec.gov/cgi-bin/browse-edgar?CIK=' + ticker + '&Find=Search&owner=exclude&action'
+                                                                               '=getcompany&start=0&count=100')
 soup = BeautifulSoup(page.content, 'html.parser')
-
+companyName = soup.find(class_ = "companyName").findNext
+print(companyName)
 tenkURL = soup.find(text="10-K").find_next('a')['href']
 #tenkURL = tenk['href']
 tenkpage = requests.get('https://www.sec.gov'+ tenkURL)
-print(tenkURL)
+print('https://www.sec.gov'+ tenkURL)
 soup = BeautifulSoup(tenkpage.content, 'html.parser')
 tenkDocURL = soup.find(text="10-K").findNext('a')['href']
+tenkDocURL = soup.find(class_='blueRow').findPrevious('a')['href']
 #tenkDocUrl = tenkDoc['href']
-print(tenkDocURL)
+print('https://www.sec.gov' + tenkDocURL)
 tenkDOCpage = requests.get('https://www.sec.gov'+ tenkDocURL)
 soup = BeautifulSoup(tenkDOCpage.content, 'html.parser')
 
